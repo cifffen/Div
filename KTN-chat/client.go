7848e25244d7b81a029cfbd5a05c5be9aeefc9ce
cifffen <christianfsaetre@gmail.com>
2014-03-15 15:06:41 +0100

    Client in go ok

--- KTN-chat/client.go
+++ KTN-chat/client.go
@@ -5,6 +5,8 @@ import(
 	"encoding/json"
 	"fmt"
 	"time"
+	"os"
+	"log"
 )
 const (
 	RECV_BUF_LEN = 1024
@@ -21,53 +23,101 @@ const (
 )
 
 func main()(){
-	 //map[string]string{"request": "login","username":"ciffen"}
 	serverAddr, _ := net.ResolveTCPAddr("tcp", "192.168.1.4:9999")
-	con, _ := net.DialTCP("tcp", nil, serverAddr);
+	con, err := net.DialTCP("tcp", nil, serverAddr);
+	fmt.Printf("Connected to server. \n")
+	if err != nil{
+		log.Printf("Error: %v",err)
+		os.Exit(1)
+	}
 	buf := make([]byte, RECV_BUF_LEN)
-	var fromUser string
-	for {
-		msg := make(map[string]interface{})
-		go func(){
+	fmt.Printf("Welcome, good sir, to the Chat of Awesomeness!\n")
+	fmt.Printf("Type a message and hit Enter to send (Type 'login' to log in; type 'logout' to log out.) \n")
+	go func(){
+		for {
 			var msgIn  map[string] interface{}
 			buf2 := make([]byte, RECV_BUF_LEN)
 			rlen,err := con.Read(buf2)
 			if err != nil {
 				fmt.Printf("Error: %v", err)
 	        }
-			json.Unmarshal(buf2[:rlen],&msgIn)
-			for k, v:= range msgIn{
-				fmt.Printf("key:%s, val: %s \n",k,v)
+			err =json.Unmarshal(buf2[:rlen],&msgIn)
+			if err != nil{
+				log.Printf("Error: %v",err)
 			}
-		}()
+			printToScreen(msgIn)
+		}
+	}()
+	for {
 		time.Sleep(time.Millisecond*100)
-		fmt.Printf("Request: ")
-		_,err:=fmt.Scanf("%s\n",&fromUser)
-		if err != nil {
+		msg := getInputFromUser()
+		buf,err =json.Marshal(msg)
+		if err!=nil {
 			fmt.Printf("Error: %v", err)
 		}
-		msg[request]=fromUser
-		if fromUser == login{
+		con.Write(buf)
+
+	}
+}
+func getInputFromUser() (msg map[string]string){
+	var fromUser string
+	fmt.Printf(": ")
+	_,err:=fmt.Scanf("%s\n",&fromUser)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	
+	msg =make(map[string]string)
+	msg[request]=message
+	switch fromUser {
+		case login:
+			msg[request]=login
 			fmt.Printf("Username: ")
 			_,err:=fmt.Scanf("%s\n",&fromUser)
 			if err != nil {
 				fmt.Printf("Error: %v", err)
 			}
 			msg[username]=fromUser
-		} else if fromUser == logout{
-		} else{
-			fmt.Printf("Message: ")
-			_,err=fmt.Scanf("%s\n",&fromUser)
-			if err != nil {
-				fmt.Printf("Error: %v", err)
-			}
-			msg[message]=fromUser
+			return 
+		case logout:
+			msg[request]=logout
+		default:
+			msg[message]=fromUser	
+	}
+	return
+}
+	
+func printToScreen(msgIn map[string] interface{}){
+	if resp, ok:= msgIn[response]; ok{
+		switch resp{
+			case login:
+				if un, unPres := msgIn[username]; unPres{
+					if errMsg, unErr := msgIn[error]; unErr{
+						fmt.Printf("Username: %s - %s \n", un, errMsg)
+					} else {
+						fmt.Printf("Your are logged in with username: %s \n", un)
+						if messages, msgsOk := msgIn[messages]; msgsOk{
+							fmt.Printf("Previous messages:\n %s",messages)
+						}
+					}
+				}
+			case message:
+				if err, msgErr := msgIn[error]; msgErr{
+					fmt.Printf("%s \n", err)
+				} else if msg, msgOk := msgIn[message]; msgOk{
+					fmt.Printf("%s \n", msg)
+				}
+			case logout:
+				if un, unPres := msgIn[username]; unPres{
+					if err, lgoErr := msgIn[error]; lgoErr{
+						fmt.Printf("User:%s %s \n", un, err)
+					} else{
+						fmt.Printf("User: %s has logged out. \n", un)
+					}
+				}
 		}
-		buf,err =json.Marshal(msg)
-		if err!=nil {
-			fmt.Printf("Error: %v", err)
-		}
-		//fmt.Printf("%d", msg)
-		con.Write(buf)
 	}
 }
+					
+					
+			
